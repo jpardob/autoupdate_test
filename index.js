6b57217668515b253834c173b5faaa8a01ed9ad1
JPardo <ju.pardob@alumnos.duoc.cl>
2024-02-21 17:40:04 -0300

    u1

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
+require("dotenv").config()
 const {exec} = require("child_process")
-exec("git pull",(error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
-console.log("this is a new version 2")
+const cluster = require("cluster")
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+const axios = require("axios")
+const fs = require("fs")
+
+const bot = new Telegraf(process.env.telegramToken)
+
+if(cluster.isWorker){
+    bot.hears(/update/i, (ctx) => {
+        exec("git pull",(error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+        console.log("this is a new version 2")
+        process.exit();
+    })
+    bot.on(message("document"),(ctx)=>{
+        let document = ctx.update.message.document;
+        ctx.telegram.getFileLink(document.file_id).then(url => {    
+            axios({url, responseType: 'stream'}).then(response => {
+                return new Promise((resolve, reject) => {
+                    response.data.pipe(fs.createWriteStream(`${__dirname}/files/${document.file_name}`))
+                                .on('finish', () => {
+                                    console.log("saved");
+                                    ctx.reply("saved")
+                            })
+                                .on('error', e => console.log(e))
+                        });
+                    })
+        }) 
+    })    
+    bot.launch()
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}
+if(cluster.isMaster){
+    cluster.fork();
+    cluster.on('exit', function(worker, code, signal) {
+        cluster.fork();
+    });
+}
