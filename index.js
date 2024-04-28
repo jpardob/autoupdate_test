209052633454577a533f28c17119c0987da22e36
JPardo <ju.pardob@alumnos.duoc.cl>
2024-04-28 10:19:48 -0400

    f

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ getCover = async(link) =>{
         }},
         {match:/monoschinos2\.com/i, finder:async(link)=>{
                 let rawHtml = await getHTML(link)
-                let cover = rawHtml.match(/(?<=<div class="lozad bg-secondary">[\S,\s]*<img src=")(.*)\.jpg/g);
+                let cover = rawHtml.match(/(?<=<img class="lozad bg-secondary[\S,\s]* src=")(.*)\.jpg/g);
                 return (cover && cover[0])?`${cover[0]}`:""
         }}
     ]
