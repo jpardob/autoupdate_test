0b0fcfb12691f7af06849b01da15ff2e372a5d8e
JPardo <ju.pardob@alumnos.duoc.cl>
2024-10-22 08:42:43 -0300

    allow null on anime

--- models/temp.js
+++ models/temp.js
@@ -11,7 +11,7 @@ module.exports=(sequelize)=>
             },
             anime:{
                 type:DataTypes.INTEGER,
-                allowNull:false
+                allowNull:true
             },
             name:{
                 type:DataTypes.STRING,
